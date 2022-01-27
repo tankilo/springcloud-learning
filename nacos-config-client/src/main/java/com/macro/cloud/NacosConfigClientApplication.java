f385eb6686751f92880742b76dbd6cc840e55aca
tankilo <tankilo@126.com>
2022-01-27 19:30:22 +0800

    update version

--- nacos-config-client/src/main/java/com/macro/cloud/NacosConfigClientApplication.java
+++ nacos-config-client/src/main/java/com/macro/cloud/NacosConfigClientApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class NacosConfigClientApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(NacosConfigClientApplication.class, args);
     }
